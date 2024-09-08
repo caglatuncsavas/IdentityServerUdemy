b12f00a2b35c19b8977092fb1255fc5719cdc6a4
Çağla Savaş <cagla.savas@btcturk.com>
2024-09-08 18:02:26 +0300

    Add CustomUserRepository and ICustomUserRepository

---
+++ UdemyIdentityServer.AuthServer/Repository/CustomUserRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UdemyIdentityServer.AuthServer.Models;
+
+namespace UdemyIdentityServer.AuthServer.Repository
+{
+    public class CustomUserRepository : ICustomUserRepository
+    {
+        private readonly CustomDbContext _context;
+
+        public CustomUserRepository(CustomDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<CustomUser> FindByEmail(string email)
+        {
+           return await _context.CustomUsers.FirstOrDefaultAsync(x => x.Email == email);
+        }
+
+        public async Task<CustomUser> FindById(int id)
+        {
+            return await _context.CustomUsers.FindAsync(id);
+        }
+
+        public async Task<bool> Validate(string email, string password)
+        {
+            return await _context.CustomUsers.AnyAsync(x => x.Email == email && x.Password == password);
+        }
+    }
+}
